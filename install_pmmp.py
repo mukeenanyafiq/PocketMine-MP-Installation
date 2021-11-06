8d608d6d851892784e1413eceac7db08a306bf03
keenanyafiqy <93072673+keenanyafiqy@users.noreply.github.com>
2021-11-07 06:08:14 +0700

    Update install_pmmp.py

--- install_pmmp.py
+++ install_pmmp.py
@@ -3,14 +3,18 @@ import os
 import time
 import sys
 
-print("Welcome to PocketMine-MP Installation")
+os.system ("clear")
 def slowprint(s):
-    for c in s + '\n' :
+    for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(5. / 100)
 
+slowprint("Welcome to PocketMine-MP Installation (64 bit)")
+slowprint("-------------------------------------")
 slowprint("Created by Keenan Yafiq (keenanyafiqy)")
-choice = input("Do you want to start the installation? [y/n]")
+slowprint("Executed from Python")
+slowprint("This installation will install PocketMine-MP v3.25.2")
+choice = input("Do you want to start the installation? [y/n]: ")
 if choice == 'n' : sys.exit()
 if choice == 'y' : os.system ("apt update")
