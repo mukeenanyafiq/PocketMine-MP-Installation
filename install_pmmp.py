a6c745e0c7ae41cf899d1ca299f6c747e5beec29
keenanyafiqy <93072673+keenanyafiqy@users.noreply.github.com>
2021-11-07 06:46:38 +0700

    Update install_pmmp.py

--- install_pmmp.py
+++ install_pmmp.py
@@ -19,16 +19,13 @@ choice = input("Do you want to start the installation? [y/n]: ")
 if choice == 'n' : sys.exit()
 if choice == 'y' : os.system ("apt update")
 
-os.system ("cd")
 os.system ("mkdir PocketMine-MP")
 os.system ("cd PocketMine-MP")
 os.system ("wget https://github.com/pmmp/PocketMine-MP/releases/download/3.25.2/PocketMine-MP.phar")
 os.system ("wget https://github.com/pmmp/PocketMine-MP/releases/download/3.25.2/start.sh")
-os.system ("cd")
 
 choice = input("Folders and Files has been created, would you like to start the server now? [y/n]: ")
 if choice == 'n' : sys.exit()
-if choice == 'y' : os.system ("cd PocketMine-MP")
+if choice == 'y' : slowprint("Starting the server... If any problems occured, try some fixes you may can look on Google")
 
-slowprint("Starting the server... If any problems occured, try some fixes you may can look on Google")
 os.system ("./start.sh")
