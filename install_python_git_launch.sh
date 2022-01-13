167cede28228688d43ddb34149dd6f313f2284dd
Keenan Yafiq <accbruh0@gmail.com>
2022-01-13 13:03:19 +0700

    Update install_python_git_launch.sh

--- install_python_git_launch.sh
+++ install_python_git_launch.sh
@@ -1,6 +1,12 @@
 pkg install python
 pkg install python2
 pkg install git
+pkg install getconf
+
+if [ `getconf LONG_BIT` == "32" ]; then
+	echo "This setup is needed 32-bit operating system!"
+        exit 1
+fi
 
 clear
 
