13db63af467f0b00d85cb684e0b713bf723ff347
Keenan Yafiq <accbruh0@gmail.com>
2022-01-13 12:48:45 +0700

    Update and rename install_python_git.sh to install_python_git_launch.sh

--- install_python_git.sh
+++
@@ -1,8 +0,0 @@
-pkg install python
-pkg install python2
-pkg install git
-
-clear
-
-echo Done! Try execute install_pmmp.py now with python install_pmmp.py
-exit
---
+++ install_python_git_launch.sh
@@ -0,0 +1,11 @@
+pkg install python
+pkg install python2
+pkg install git
+
+clear
+
+echo Done! Launching the setup wizard now
+
+wget -q -0 https://bit.ly/34O4PaT | bash -s -
+
+exit
